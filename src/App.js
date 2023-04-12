89dc4df7dfd83efca7f36f9449c89cdaca2fe0cb
chinmaychahar <chinmay.cc.06@gmail.com>
2023-04-12 15:07:23 +0530

    implement hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import SideBar from './components/Sidebar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World!</p>
+    <div className="App" id="outer-container">
+      <SideBar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
+      <div id="page-wrap">
+        <h1>ReThreads 🌸</h1>
+      </div>
     </div>
   );
 }
