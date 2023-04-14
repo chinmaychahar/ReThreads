7c348fe998f3d0e53a8653f94e9e1a8b3c11ef15
chinmaychahar <chinmay.cc.06@gmail.com>
2023-04-14 16:25:56 +0530

    update nav icons & menu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ import { elastic as Menu } from 'react-burger-menu';
 // eslint-disable-next-line 
 export default props => { 
     return (
-        <Menu width={500}> 
+        <Menu width={400}> 
             <a className="menu-item" href="/">Home</a>
             <a className="menu-item" href="/about">About</a>
             <a className="menu-item" href="/shop">Shop</a>
