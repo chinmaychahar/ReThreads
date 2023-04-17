c9578de6eb85f596badebd862bbe81e04ecb6eb8
chinmaychahar <chinmay.cc.06@gmail.com>
2023-04-17 17:33:16 +0530

    clear project template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import SideBar from './components/Sidebar';
-import {TfiHeart} from 'react-icons/tfi';
-import {BsCart2} from 'react-icons/bs';
 import './App.css';
 
 function App() {
   return (
-    <div className="App" id="outer-container">
-      <SideBar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
-      <div id="page-wrap">
-      </div>
-      <div>
-      <h2 align="right" id="home-icons"><TfiHeart/>&emsp;<BsCart2/></h2> 
-      </div>
+    <div >
+      <h1>ReThreads!</h1>
     </div>
   );
 }
