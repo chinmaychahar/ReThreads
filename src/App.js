d3826658ce3ee6faa5fc495b9e33d25d572ba807
kanika-crypto <68729863+KanikaRaheja@users.noreply.github.com>
2023-04-22 00:07:11 +0530

    worked on feature section

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import './App.css';
 import Banner from './components/banner/'
 import { BrowserRouter,Routes, Route } from "react-router-dom";
+import featureSec from './components/featureSection/section.js';
+
 function App() {
+
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Banner />}>
-        {/* <Route index element={<Home />} />
-        <Route path="blogs" element={<Blogs />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<NoPage />} /> */}
-      </Route>
-    </Routes>
-  </BrowserRouter>
+    <div >
+      <Banner />
+    </div>
   );
 }
 
